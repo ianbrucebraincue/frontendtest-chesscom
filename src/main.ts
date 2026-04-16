01abe1a947fcdfa2888996df19811d634ec4c408
Ian Bruce <ianbruce.braincue@gmail.com>
2026-04-16 15:38:14 +0100

    add: styles folder structure. removed: unused assets, styles, and components

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import './assets/main.css'
+import './assets/styles/normalize.css'
+import './assets/styles/global.scss'
+import './assets/styles/main.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
